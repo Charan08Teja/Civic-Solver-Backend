cabfb1a26814ac39230dcc3ac148baf252ee5d47
Charan08Teja <charangudipudi93@gmail.com>
2026-04-26 21:51:40 +0530

    Added Cloudinary image upload

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -1,16 +1,5 @@
-const multer = require('multer');
-const path = require('path');
-
-// storage config
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    const uniqueName = Date.now() + path.extname(file.originalname);
-    cb(null, uniqueName);
-  }
-});
+const multer = require("multer");
+const { storage } = require("../../cloudinary");
 
 const upload = multer({ storage });
 
