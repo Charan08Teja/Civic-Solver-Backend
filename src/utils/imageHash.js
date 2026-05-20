c719c2b6a793d651bd4ccdec6afbbbcd2dce3413
Charan08Teja <charangudipudi93@gmail.com>
2026-05-20 19:10:32 +0530

    Fix Cloudinary image hashing

--- src/utils/imageHash.js
+++ src/utils/imageHash.js
@@ -1,11 +1,55 @@
 const { imageHash } = require('image-hash');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 
-const generateHash = (imagePath) => {
-  return new Promise((resolve, reject) => {
-    imageHash(imagePath, 16, true, (error, data) => {
-      if (error) return reject(error);
-      resolve(data);
-    });
+const generateHash = async (imageUrl) => {
+
+  return new Promise(async (resolve, reject) => {
+
+    try {
+
+      // Temporary file path
+      const tempFilePath = path.join(
+        __dirname,
+        `temp-${Date.now()}.jpg`
+      );
+
+      // Download image from Cloudinary
+      const response = await axios({
+        url: imageUrl,
+        method: 'GET',
+        responseType: 'stream'
+      });
+
+      // Save locally
+      const writer = fs.createWriteStream(tempFilePath);
+
+      response.data.pipe(writer);
+
+      writer.on('finish', () => {
+
+        // Generate hash from local temp image
+        imageHash(tempFilePath, 16, true, (error, data) => {
+
+          // Delete temp file
+          fs.unlinkSync(tempFilePath);
+
+          if (error) {
+            return reject(error);
+          }
+
+          resolve(data);
+        });
+      });
+
+      writer.on('error', (err) => {
+        reject(err);
+      });
+
+    } catch (error) {
+      reject(error);
+    }
   });
 };
 
